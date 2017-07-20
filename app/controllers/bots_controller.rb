c3a508f4f61c784d6fb75c3d6b268c51e7b17e26
Joshua Ling <joshling1919@gmail.com>
2017-07-20 12:11:52 +0800

    Finish setting up initial iteration of bot

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -3,20 +3,22 @@ require 'uri'
 
 class BotsController < ApplicationController
 	def accept_command
-		if params["group_id"] == ENV["group_id"] || 
-			params["group_id"] == ENV["test_group_id"] && 
-			params["sender_type"] == "user"
-
+		puts params
+		if params["sender_type"] == "user"
 			text_arr = params["text"].downcase.split(" ")
+			last_i = text_arr.length - 1
+			if text_arr.include?("bet") || text_arr[last_i].include?("bet")
 
-			if text_arr.include?("bet")
-				post_obj = {}
-				post_obj["bot_id"] = ENV["test_bot_id"]
-				post_obj["text"] = "Did someone say bet?"
+				if params["group_id"] == ENV["group_id"] 
+					bot_id = ENV["bot_id"]
+				elsif	params["group_id"] == ENV["test_group_id"]
+					bot_id = ENV["test_bot_id"]
+				end
+
 				uri = URI('https://api.groupme.com/v3/bots/post')
 				res = Net::HTTP.post_form(
 					uri, 
-					"bot_id" => ENV["test_bot_id"],
+					"bot_id" => bot_id,
 					"text" => "Did someone say bet?"
 				)
 				puts res.body
